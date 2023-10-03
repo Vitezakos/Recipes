4d4264db5ad13749df50854e107dd5704f565027
Vitezakos <vitezakosh@gmail.com>
2023-10-03 11:46:40 +0200

    Multiple entries of the same ingredient now merge together adding up their sum quantity

--- src/index.ts
+++ src/index.ts
@@ -93,22 +93,75 @@ function saveData() {
 function shoppingElement() {
   const itemName = document.querySelector(".items");
   let listingName = JSON.parse(localStorage.getItem("listing")!);
-  const itemList = document.querySelector(".itemList");
-  if (localStorage.getItem("listing")) {
-    console.log(listingName);
-    for (let i = 0; i < listingName.length; i++) {
-      const li = document.createElement("li");
-      li.innerText = listingName[i].name;
-      itemName?.appendChild(li);
-      for (let j = 0; j < listingName[i].element.length; j++) {
-        const li2 = document.createElement("li");
-        li2.innerText = listingName[i].element[j];
-        itemList?.appendChild(li2);
+  const itemList = document.querySelector(".itemList") as HTMLUListElement;
+  if (itemName!.innerHTML == "" && itemList!.innerHTML == "") {
+    if (localStorage.getItem("listing")) {
+      for (let i = 0; i < listingName.length; i++) {
+        const li = document.createElement("li");
+        li.innerText = listingName[i].name;
+        itemName?.appendChild(li);
+        for (let j = 0; j < listingName[i].element.length; j++) {
+          const li2 = document.createElement("li");
+          li2.innerText = listingName[i].element[j];
+          itemList?.appendChild(li2);
+        }
       }
+    } else {
+      itemName!.innerHTML = "";
+      itemList!.innerHTML = "";
     }
-  } else {
+  } else if (
+    (itemName!.innerHTML !== "" || itemList!.innerHTML !== "") &&
+    !localStorage.getItem("listing")
+  ) {
     itemName!.innerHTML = "";
     itemList!.innerHTML = "";
+  }
+  let text = [] as any;
+  itemList!.childNodes.forEach((e) => {
+    let elements = e.textContent?.split(" ");
+    text.push(elements);
+  });
+  for (let i = 0; i < text.length; i++) {
+    let check = 0;
+    let newValue = "";
+
+    for (let j = 0; j < text.length; j++) {
+      if (text[i][0] == text[j][0]) {
+        check = check + 1;
+        if (check > 1) {
+          if (
+            text[i][1].slice(text[i][1].length - 1) ==
+            text[j][1].slice(text[j][1].length - 1)
+          ) {
+            let firstValue = text[i][1].slice(0, text[i][1].length - 1);
+            let secondValue = text[j][1].slice(0, text[i][1].length - 1);
+            let firstNumber = +firstValue;
+            let secondNumber = +secondValue;
+            let newNumber = firstNumber + secondNumber;
+            let newNumberAsString = newNumber.toString();
+            newValue =
+              newNumberAsString + text[i][1].slice(text[i][1].length - 1);
+            text[i][1] = newValue;
+          } else {
+            newValue =
+              text[i][1].slice(0, text[i][1].length) +
+              " " +
+              "+" +
+              " " +
+              text[j][1].slice(0, text[j][1].length);
+            text[i][1] = newValue;
+          }
+          let liToChange = itemList.children[i];
+          if (liToChange) {
+            liToChange.textContent = text[i][0] + " " + newValue;
+          }
+          if (itemList.children[j]) {
+            itemList.children[j].remove();
+          }
+        }
+      }
+    }
   }
 }
 document.querySelector(".myList")?.addEventListener("click", shoppingElement);
